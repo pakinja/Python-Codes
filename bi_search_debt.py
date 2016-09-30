29a8ba6c1d67fa35e9843bb67fa156a379922fde
Francisco Jaramillo Aguilar <francisco.jaramilloaguilar@gmail.com>
2016-09-29 21:37:25 -0500

    Bisection Search fix pay (credit card)
    We are searching for the smallest monthly payment such that we can pay off the entire balance within a year.

---
+++ bi_search_debt.py
@@ -0,0 +1,24 @@
+# Paste your code into this box
+lower = balance / 12.0
+upper = balance*((1 + (annualInterestRate)/12)**12)/12.0
+p = (lower + upper)/2
+epsilon = 0.01
+
+#print(lower, upper)    
+j=0
+for j in range(30):
+    bal_1 = [balance]               
+    for i in range(12):
+        bal_1.append((bal_1[i] - p) +
+        (annualInterestRate/12)*(bal_1[i] - p))
+        #print(bal_1[i], p)
+        #print[bal_1[-1]]
+                                  
+    if bal_1[-1] < 0:
+        upper = p
+        #print(upper)
+    elif bal_1[-1] > 0:
+        lower = p
+    p = (lower + upper)/2
+    j += 1
+print(round(p,2))
